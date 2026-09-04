a56d5f31d9c78b927afdc35b4388089144df9599
GAGAN1950 <gagan748318@gmail.com>
2026-09-04 10:52:06 +0530

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -6,4 +6,14 @@ if a:
         st.chat_message("ai").write("hello")
     elif a.lower()=="bye":
         st.chat_message("ai").write("goodbye")
+          elif a.lower()=="how are you":
+        st.chat_message("ai").write("i am fine!")
+     elif a.lower()=="what is your name":
+        st.chat_message("ai").write("i am Gagan.R")
+          elif a.lower()=="introduce yourself":
+        st.chat_message("ai").write("i am gagan from ssmrv college and i have pursued bca with cybersecurity and cloud architecture")
+ elif a.lower()=="thanks":
+        st.chat_message("ai").write("no problem!")
+    
+    
 
